a59ff0330cc04735f5ae284cf12ca0dbbcaa606c
Diluxshan <diluxv@gmail.com>
2022-10-12 21:11:20 +0530

    comment

--- main.c
+++ main.c
@@ -13,11 +13,16 @@ int Multiple(int a, int b){
     return a * b;
 }
 
+int Devision(int a, int b){
+    return a / b;
+}
+
 int main() {
     printf("C basics and final one\n");
     printf("Addition is         : %d\n", Add(5, 8));
     printf("Subtraction is      : %d\n", Subb(6.5, 0.5));
     printf("Myltiplation is     : %d\n", Multiple(2, 4));
+    printf("Myltiplation is     : %d\n", Multiple(2, 4)); //hello
     printf("All the functions are working fine with the edits");
 
 
